2456a874daef38cf928f6361f461fa88ab537a7b
vishal gaur <vishalgaur752@gmail.com>
2025-02-17 14:53:52 +0530

    JavaLoops

---
+++ WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i <=100) {
+            System.out.print("Hello World" + " ");
+            i++;
+        }
+    }
+}
